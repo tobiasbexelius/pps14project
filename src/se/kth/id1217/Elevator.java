d0a05fc622338a131fb4fe38dc459762919b0ece
Victor Koronen <koronen@kth.se>
2014-03-09 14:50:18 +0100

    Add JavaDoc to Elevator, synchronize methods

--- src/se/kth/id1217/Elevator.java
+++ src/se/kth/id1217/Elevator.java
@@ -1,9 +1,10 @@
 package se.kth.id1217;
 
 /**
- * Represents an elevator.
+ * Represents an elevator. Holds data about id, position, the door and speed.
  */
 public class Elevator {
+
     public static final double DEFAULT_POSITION = 0.0;
     public static final double DEFAULT_SPEED = 0.000157;
     private static final double DELTA = 0.01;
@@ -13,6 +14,12 @@ public class Elevator {
     private double position;
     private double speed;
 
+    /**
+     * Instantiates an elevator.
+     * 
+     * @param id
+     *            The ID of this elevator.
+     */
     public Elevator(int id) {
         this.id = id;
         position = DEFAULT_POSITION;
@@ -20,55 +27,135 @@ public class Elevator {
         doorOpen = false;
     }
 
-    public void closeDoor() {
+    /**
+     * Marks the door as closed.
+     */
+    public synchronized void closeDoor() {
         doorOpen = false;
     }
 
-    public int getFloor() {
+    /**
+     * Returns the number of the closest floor.
+     * 
+     * @return The number of the closest floor.
+     */
+    public synchronized int getFloor() {
         return (int) position;
     }
 
+    /**
+     * Returns the ID of this elevator.
+     * 
+     * @return The ID of this elevator.
+     */
     public int getId() {
         return id;
     }
 
-    public double getPosition() {
+    /**
+     * Returns the current position of this elevator.
+     * 
+     * @return The current position.
+     */
+    public synchronized double getPosition() {
         return position;
     }
 
-    public double getSpeed() {
+    /**
+     * Returns the current speed of this elevator.
+     * 
+     * @return The current speed.
+     */
+    public synchronized double getSpeed() {
         return speed;
     }
 
-    public boolean isAtFloor(int floor) {
+    /**
+     * Returns <tt>true</tt> iff the elevator is at the specified floor (taking
+     * into consideration a slight margin of error). Returns <tt>false</tt>
+     * otherwise.
+     * 
+     * @param floor
+     *            The number of the floor.
+     * @return <tt>true</tt> iff the elevator is at the specified floor.
+     */
+    public synchronized boolean isAtFloor(int floor) {
         return Math.abs(position - floor) < DELTA;
     }
 
-    public boolean isAtFloor() {
+    /**
+     * Returns <tt>true</tt> iff the elevator is at any floor (taking into
+     * consideration a slight margin of error). Returns <tt>false</tt>
+     * otherwise.
+     * 
+     * @param floor
+     *            The number of the floor.
+     * @return <tt>true</tt> iff the elevator is at any floor.
+     */
+    public synchronized boolean isAtFloor() {
         return Math.abs(position - Math.round(position)) < DELTA;
     }
 
-    public boolean isDoorOpen() {
+    /**
+     * Returns <tt>true</tt> iff the door is marked as open. Returns
+     * <tt>false</tt> otherwise.
+     * 
+     * @return <tt>true</tt> iff the door is marked as open.
+     */
+    public synchronized boolean isDoorOpen() {
         return doorOpen;
     }
 
-    public void openDoor() {
+    /**
+     * Marks the door as open.
+     */
+    public synchronized void openDoor() {
         doorOpen = true;
     }
 
-    public void setPosition(double position) {
+    /**
+     * Updates the position of this elevator.
+     * 
+     * @param position
+     *            The new position.
+     */
+    public synchronized void setPosition(double position) {
         this.position = position;
     }
 
-    public void setSpeed(double speed) {
+    /**
+     * Updates the speed of this elevator.
+     * 
+     * @param speed
+     *            The new speed.
+     */
+    public synchronized void setSpeed(double speed) {
         this.speed = speed;
     }
 
-    public boolean isFloorAbove(int floor) {
+    /**
+     * Returns <tt>true</tt> iff the given floor is above the elevator (taking
+     * into consideration a slight margin of error). Returns <tt>false</tt>
+     * otherwise.
+     * 
+     * @param floor
+     *            The number of the floor.
+     * @return <tt>true</tt> iff the given floor is above the elevator.
+     */
+    public synchronized boolean isFloorAbove(int floor) {
         return floor > position + DELTA;
     }
 
-    public boolean isFloorBelow(int floor) {
+    /**
+     * Returns <tt>true</tt> iff the given floor is below the elevator (taking
+     * into consideration a slight margin of error). Returns <tt>false</tt>
+     * otherwise.
+     * 
+     * @param floor
+     *            The number of the floor.
+     * @return <tt>true</tt> iff the given floor is below the elevator.
+     */
+    public synchronized boolean isFloorBelow(int floor) {
         return floor < position - DELTA;
     }
 
