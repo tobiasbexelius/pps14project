716ab68147f8ccb48af4ebaeacbe684c7ad9bfa0
Victor Koronen <koronen@kth.se>
2014-03-11 16:12:15 +0100

    Add JavaDoc to ElevatorController

--- src/se/kth/id1217/ElevatorController.java
+++ src/se/kth/id1217/ElevatorController.java
@@ -48,28 +48,43 @@ public class ElevatorController implements Runnable {
         emergencyStop = false;
     }
 
+    /**
+     * Heuristic to calculate the "cost" of serving a given floor button press.
+     *
+     * @param fbpd
+     *            The given floor button press.
+     * @return A cost.
+     */
     public double costToServe(FloorButtonPressDesc fbpd) {
-        double cost = 0.0;
         final int floorTo = fbpd.getFloor();
 
+        // Cost is initially 0.
+        double cost = 0.0;
+
+        // Reduce cost for elevator stationary at requested floor.
         if (elevator.isAtFloor(floorTo) && commandQueue.isEmpty()
                 && elevator.isDoorOpen()) {
             cost += BONUS_STOPPED_AT_FLOOR;
         }
 
+        // Reduce cost for elevator already tasked with serving an identical
+        // request.
         if (commandQueue.contains(new FloorCommand(fbpd.getFloor(), fbpd
                 .getType()))) {
             cost += BONUS_COMMAND_IN_QUEUE;
         }
 
         if (isActive()) {
+            // Add cost for being active.
             cost += COST_ACTIVE;
 
+            // Add cost for being a request in the opposite direction.
             if ((goingUp() && floorTo < elevator.getPosition())
                     || (goingDown() && floorTo > elevator.getPosition())) {
                 cost += COST_WRONG_DIRECTION;
             }
 
+            // Reduce cost for being a request along the current route.
             if (floorAlongCurrentRoute(floorTo)) {
                 if ((goingUp() && fbpd.getType() == FloorButtonType.GoingUp)
                         || (goingDown() && fbpd.getType() == FloorButtonType.GoingDown)) {
@@ -78,11 +93,19 @@ public class ElevatorController implements Runnable {
             }
         }
 
+        // Add cost equal to the distance.
         cost += Math.abs(floorTo - elevator.getPosition());
 
         return cost;
     }
 
+    /**
+     * Is the given floor along the current route of this elevator?
+     *
+     * @param floor
+     *            The given floor.
+     * @return T/F
+     */
     private boolean floorAlongCurrentRoute(int floor) {
         int turningFloor = getTurningFloor();
         boolean floorAlongRouteUp = goingUp() && turningFloor >= floor
@@ -92,6 +115,11 @@ public class ElevatorController implements Runnable {
         return floorAlongRouteUp || floorAlongRouteDown;
     }
 
+    /**
+     * Is the elevator currently heading down?
+     *
+     * @return
+     */
     private boolean goingDown() {
         FloorCommand fc = getNextFloorCommand();
         return fc != null && elevator.isFloorBelow(fc.getFloor());
@@ -108,20 +136,43 @@ public class ElevatorController implements Runnable {
         return null;
     }
 
+    /**
+     * Is the elevator active? I.e. does it have any commands in its queue?
+     *
+     * @return
+     */
     private boolean isActive() {
         return !commandQueue.isEmpty();
     }
 
+    /**
+     * Is the elevator currently heading up?
+     *
+     * @return
+     */
     private boolean goingUp() {
         FloorCommand fc = getNextFloorCommand();
         return fc != null && elevator.isFloorAbove(fc.getFloor());
     }
 
+    /**
+     * Is the elevator currently stopped?
+     *
+     * @return
+     */
     private boolean isStopped() {
         return currentMotorAction == MotorAction.MotorStop;
     }
 
+    /**
+     * Ask this elevator to serve the given request.
+     *
+     * @param floorCommand
+     *            The given request.
+     */
     public void addCommand(FloorCommand floorCommand) {
+        // Don't add the command to the queue if it's already present in the
+        // queue, or if this elevator is currently stopped on the same floor.
         if (!commandQueue.contains(floorCommand)
                 && !(elevator.isAtFloor(floorCommand.getFloor()) && isStopped())) {
             commandQueue.add(floorCommand);
@@ -129,6 +180,10 @@ public class ElevatorController implements Runnable {
         addCommandCalled.release();
     }
 
+    /**
+     * Tells the hardware to close the elevator door, if not already closed.
+     * Will wait a short while for the door to close.
+     */
     private void closeDoor() {
         if (elevator.isDoorOpen()) {
             hwc.handleDoor(elevator.getId(), DoorAction.DoorClose);
@@ -137,6 +192,10 @@ public class ElevatorController implements Runnable {
         }
     }
 
+    /**
+     * Waits a short while for the door to close. Scales with the speed of the
+     * elevator.
+     */
     private void waitForDoor() {
         try {
             long duration = Math.round(DOOR_MOTION_DELAY
@@ -147,11 +206,20 @@ public class ElevatorController implements Runnable {
         }
     }
 
+    /**
+     * Tells the hardware to send the elevator downwards.
+     */
     private void goDown() {
         hwc.handleMotor(elevator.getId(), MotorAction.MotorDown);
         currentMotorAction = MotorAction.MotorDown;
     }
 
+    /**
+     * Process the given command. Closes the doors, travels (might stop along
+     * the way) and opens the doors.
+     *
+     * @param floorCommand
+     */
     private void goToFloor(FloorCommand floorCommand) {
         // Always clear emergency stop when instructed to go to a new floor
         emergencyStop = false;
@@ -204,6 +272,14 @@ public class ElevatorController implements Runnable {
         }
     }
 
+    /**
+     * Finds commands in the command queue matching the given floor and the
+     * current direction of this elevator, if any.
+     *
+     * @param floor
+     *            The given floor.
+     * @return
+     */
     private List<FloorCommand> findCommandsInQueueByFloorAndCurrentDirection(
             int floor) {
         List<FloorCommand> commands = new LinkedList<FloorCommand>();
@@ -231,11 +307,18 @@ public class ElevatorController implements Runnable {
         return commands;
     }
 
+    /**
+     * Tells the hardware to send the elevator upwards.
+     */
     private void goUp() {
         hwc.handleMotor(elevator.getId(), MotorAction.MotorUp);
         currentMotorAction = MotorAction.MotorUp;
     }
 
+    /**
+     * Tells the hardware to open the elevator door, if not already open. Will
+     * wait a short while for the door to open and to let passengers in.
+     */
     private void openDoor() {
         if (!elevator.isDoorOpen()) {
             hwc.handleDoor(elevator.getId(), DoorAction.DoorOpen);
@@ -256,6 +339,7 @@ public class ElevatorController implements Runnable {
                 // Ignore
             }
 
+            // Process the floor command if there is one.
             FloorCommand command = commandQueue.peek();
             if (command != null) {
                 goToFloor(command);
@@ -263,25 +347,43 @@ public class ElevatorController implements Runnable {
         }
     }
 
+    /**
+     * Tells the hardware to send the elevator upwards.
+     */
     private void stop() {
         hwc.handleMotor(elevator.getId(), MotorAction.MotorStop);
         currentMotorAction = MotorAction.MotorStop;
     }
 
+    /**
+     * Asks the hardware for the current position of this elevator.
+     */
     private void updatePosition() {
         hwc.whereIs(elevator.getId());
     }
 
+    /**
+     * Tells the hardware to update the scale for this elevator.
+     */
     private void updateScale() {
         hwc.handleScale(elevator.getId(), elevator.getFloor());
     }
 
+    /**
+     * Trigger the emergency stop.
+     */
     public void emergencyStop() {
         emergencyStop = true;
     }
 
+    /**
+     * Calculate the floor which this elevator will turn at.
+     *
+     * @return
+     */
     private int getTurningFloor() {
         int turningFloor = -1;
+
         if (goingUp()) {
             turningFloor = Integer.MIN_VALUE;
             for (FloorCommand fc : commandQueue) {
@@ -303,7 +405,7 @@ public class ElevatorController implements Runnable {
                 }
             }
         }
-        return turningFloor;
 
+        return turningFloor;
     }
 }
